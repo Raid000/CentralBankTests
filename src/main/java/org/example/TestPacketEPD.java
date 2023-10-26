a048292702bf63c2bb58b682c75553c794ac4845
Your Name <you@example.com~>
2023-10-26 15:23:52 +0300

    FirstCommit

---
+++ src/main/java/org/example/TestPacketEPD.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestPacketEPD {
+
+    @Test
+    void hello(){
+        System.out.println("efw");
+    }
+}
